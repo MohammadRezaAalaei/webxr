945e77bcc59429b4262a8412a700a2a14d87b4d3
Mohammadreza <Mohammadrezaaalaei@gmail.com>
2023-08-06 18:25:55 +0330

    test4

--- src/scene.jsx
+++ src/scene.jsx
@@ -7,7 +7,7 @@ import React, { useRef } from 'react'
 import { useGLTF } from '@react-three/drei'
 
 export function Model(props) {
-  const { nodes, materials } = useGLTF('/public/box.gltf')
+  const { nodes, materials } = useGLTF('models/box.glb')
   return (
     <group {...props} dispose={null}>
       <mesh geometry={nodes.Cube.geometry} material={materials.Material} />
@@ -15,4 +15,4 @@ export function Model(props) {
   )
 }
 
-useGLTF.preload('/public/box.gltf')
+useGLTF.preload('models/box.glb')
